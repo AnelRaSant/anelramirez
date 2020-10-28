3ac45848e0d5db49e70885f6fbff4772d814080d
AnelRaSant <anel.rmrz.stgo7@gmail.com>
2020-10-27 18:23:37 -0600

    Updates

---
+++ precache-manifest.8a906f107f58e3677747f8189ae37a5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e02a4887d0f65fbeb71bf2bf1355c0",
+    "url": "/anelramirez/index.html"
+  },
+  {
+    "revision": "14861fe3ebf2354dabf6",
+    "url": "/anelramirez/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "adf61ae50a4712d00ab0",
+    "url": "/anelramirez/static/css/main.7f51b84a.chunk.css"
+  },
+  {
+    "revision": "14861fe3ebf2354dabf6",
+    "url": "/anelramirez/static/js/2.95de520d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/anelramirez/static/js/2.95de520d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf61ae50a4712d00ab0",
+    "url": "/anelramirez/static/js/main.2c543199.chunk.js"
+  },
+  {
+    "revision": "24ba716d42820db5e80c",
+    "url": "/anelramirez/static/js/runtime-main.36251e39.js"
+  }
+]);
